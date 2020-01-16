c337660d8f80aef004abf96798cdab3e10c407a5
mykolyn <mykolyn@gmail.com>
2020-01-16 01:42:14 -0800

    preliminary pseudo code for html and js

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,9 @@
+//retrieve input and store into variables
+
+//AJAX call to map api
+
+//AJAX call to convert locations into coordinates
+
+//AJAX call to weather api with coordinates
+
+//render route list and append weather forecast to list
